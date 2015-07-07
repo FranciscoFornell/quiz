996b1611b34467e96d2bfb1d7e50fb7c79b74c25
Francisco Fornell Vázquez <fco.fornell@gmail.com>
2015-07-07 12:17:50 +0200

    Múltiples preguntas
    Módulo 7, tema 4: Se añade una lista de preguntas. A partir de ahora se
    soportarán múltiples preguntas, almacenadas en la base de datos.

--- models/models.js
+++ models/models.js
@@ -2,7 +2,8 @@ var path = require('path');
 
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite 	DATABASE_URL = sqlite://:@:/
-var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var dbURL = process.env.DATABASE_URL||"sqlite://:@:/";
+var url = dbURL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name  = (url[6]||null);
 var user	 = (url[2]||null);
 var pwd 	 = (url[3]||null);
@@ -10,7 +11,7 @@ var protocol = (url[1]||null);
 var dialect	 = (url[1]||null);
 var port	 = (url[5]||null);
 var host	 = (url[4]||null);
-var storage	 = process.env.DATABASE_STORAGE;
+var storage	 = process.env.DATABASE_STORAGE||"quiz.sqlite";
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
@@ -32,16 +33,19 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // Exportar la definición de la tabla Quiz.
 
 //sequelize.sync() crea e inicializa la tabla de preguntas en DB
-sequelize.sync().success(function () {
-	// success(...) ejecuta el manejador una vez terminada de crear la tabla
-	Quiz.count().success(function (count){
+sequelize.sync().then(function () {
+	// then(...) ejecuta el manejador una vez terminada de crear la tabla
+	Quiz.count().then(function (count){
 		if(count === 0) {
 			Quiz.create({
 			 	pregunta: 'Capital de Italia',
 			 	respuesta: 'Roma'
-			 }).success(function(){
-			 	console.log('Base de datos inicializada');
 			 });
+			Quiz.create({
+			 	pregunta: 'Capital de Portugal',
+			 	respuesta: 'Lisboa'
+			 })
+			.then(function(){ console.log('Base de datos inicializada'); });
 		};
 	});
 });
