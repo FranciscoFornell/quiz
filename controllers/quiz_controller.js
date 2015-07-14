b7a9514226fdc519c2909f10542c12666c37b422
Francisco Fornell Vázquez <fco.fornell@gmail.com>
2015-07-14 12:23:19 +0200

    Editar preguntar (Módulo 8 tema 3)
    Se añade un botón al lado de cada pregunta en la página de la lista de
    preguntas, para acceder a un formulario que permite la edición de las
    preguntas y respuestas ya existentes, actualizando la base de datos.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -72,7 +72,33 @@ exports.create = function(req, res) {
 			} else {
 				quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
 					res.redirect('/quizes');
-				});	//Redirección HTTM (URL relativo) lista de preguntas
+				});	//Redirección HTTP (URL relativo) lista de preguntas
+			}
+		}
+	);
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload de instancia de quiz
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta  = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+// guarda en DB los campos pregunta y respuesta de quiz, previa validación
+	req.quiz.validate().then(
+		function(err){
+			if (err) {
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			} else {
+				req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+					res.redirect('/quizes');
+				});	//Redirección HTTP (URL relativo) lista de preguntas
 			}
 		}
 	);
