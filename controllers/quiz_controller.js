e01b999bab20fd19c85ffe2f57c96c0bf62294aa
Francisco Fornell Vázquez <fco.fornell@gmail.com>
2015-07-15 18:59:49 +0200

    P2P final Módulo 8: Índice temático
    Se añade un campo tema a las preguntas, que puede establecerse en la
    creación de nuevas preguntas así como editarlo en las ya existentes.
    También se permite filtrar la lista de preguntas en función del tema
    además de la búsqueda.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,9 @@
 var models = require('../models/models.js');
+var listaTemas = [['otro', 'Otro'],
+				  ['humanidades', 'Humanidades'],
+				  ['ocio', 'Ocio'],
+				  ['ciencia', 'Ciencia'],
+				  ['tecnologia', 'Tecnología']];
 
 // Autoload - factoriza el código si la ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
@@ -15,16 +20,29 @@ exports.load = function(req, res, next, quizId) {
 // Get /quizes
 exports.index = function(req, res) {
 	var searchLike = '%';
-	var queryParams = { order: 'pregunta ASC'};
+	var queryParams = { order: 'pregunta ASC', where: {} };
 
 	if(req.query.search){
 		searchLike = '%' + req.query.search.toLowerCase().replace(/[ ]+/g,'%')+ '%';
-		queryParams.where = ["lower(pregunta) like ?", searchLike];
+		//Compruebo si se está en entorno de desarrollo porque en SQLITE el like es
+		//case insensitive. Si no se está en desarrollo y se está usando POSTGRES,
+		//se usa ilike en lugar de like para que sea case insensitive. Si no se usara
+		//este if, SQLITE no soporta ilike y fallaría.
+		if (!process.env.NODE_ENV||process.env.NODE_ENV==='development'){
+			queryParams.where.pregunta = {like: searchLike};	
+		} else {
+			queryParams.where.pregunta = {ilike: searchLike};
+		}
+	}
+	if(req.query.temas){
+		queryParams.where.tema = req.query.temas;
 	}
 	models.Quiz.findAll(queryParams).then(function(quizes) {
 		res.render('quizes/index.ejs', {
 			quizes: quizes,
 			search: req.query.search,
+			listaTemas: listaTemas,
+			temasSelec: req.query.temas,
 			errors: []
 		});
 	}).catch(function(error){
@@ -54,12 +72,12 @@ exports.answer = function(req, res) {
 exports.new = function(req, res) {
 	var quiz = models.Quiz.build( // crea objeto quiz
 		{
-			pregunta: "Pregunta",
-			respuesta: "Respuesta"
+			pregunta: "",
+			respuesta: ""
 		}
 	);
 
-	res.render('quizes/new', {quiz: quiz, errors: []});
+	res.render('quizes/new', {quiz: quiz, listaTemas: listaTemas, errors: []});
 };
 
 // POST /quizes/create
@@ -70,9 +88,13 @@ exports.create = function(req, res) {
 	quiz.validate().then(
 		function(err){
 			if (err) {
-				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+				res.render('quizes/new', {
+					quiz: quiz,
+					listaTemas: listaTemas,
+					errors: err.errors
+				});
 			} else {
-				quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
 					res.redirect('/quizes');
 				});	//Redirección HTTP (URL relativo) lista de preguntas
 			}
@@ -84,21 +106,26 @@ exports.create = function(req, res) {
 exports.edit = function(req, res) {
 	var quiz = req.quiz; // autoload de instancia de quiz
 
-	res.render('quizes/edit', {quiz: quiz, errors: []});
+	res.render('quizes/edit', {quiz: quiz, listaTemas: listaTemas, errors: []});
 };
 
 // PUT /quizes/:id
 exports.update = function(req, res) {
-	req.quiz.pregunta  = req.body.quiz.pregunta;
-	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.pregunta  	= req.body.quiz.pregunta;
+	req.quiz.respuesta 	= req.body.quiz.respuesta;
+	req.quiz.tema 		= req.body.quiz.tema;
 
 // guarda en DB los campos pregunta y respuesta de quiz, previa validación
 	req.quiz.validate().then(
 		function(err){
 			if (err) {
-				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+				res.render('quizes/edit', {
+					quiz: req.quiz,
+					listaTemas: listaTemas,
+					errors: err.errors
+				});
 			} else {
-				req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				req.quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
 					res.redirect('/quizes');
 				});	//Redirección HTTP (URL relativo) lista de preguntas
 			}
--- models/quiz.js
+++ models/quiz.js
@@ -11,6 +11,21 @@ module.exports = function(sequelize, DataTypes) {
 			respuesta: {
 				type: DataTypes.STRING,
 				validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+			} ,
+			tema: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: {msg: "-> Falta Tema"},
+					isIn: {
+						args: [['otro',
+								'humanidades',
+								'ocio',
+								'ciencia',
+								'tecnologia',
+								'']],
+						msg: "-> Tema inválido"
+					}
+				}
 			}
 		}
 	);
